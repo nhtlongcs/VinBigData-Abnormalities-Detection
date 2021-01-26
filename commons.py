0d19d9f22ce198a77f8375eab10e8c9cf088d343
Kay Kieran <kayp.kieran@gmail.com>
2021-01-26 15:52:48 +0700

    add coco keep ratio

--- commons.py
+++ commons.py
@@ -60,9 +60,18 @@ class YoloDataset:
             )
 
 class CocoDataset:
-    def __init__(self, df: pd.DataFrame):
+    def __init__(self, df: pd.DataFrame, img_size: int = 512, keep_ratio: bool = False):
         self.df = df
+        self.img_size = img_size
+        self.keep_ratio = keep_ratio
         self.ignore_classes = IGNORE_CLASSES
+        self.preprocess()
+
+    def preprocess(self):
+        self.df['x_min'] = self.df.apply(lambda row: 0 if row.class_id==14 else row.x_min, axis=1)
+        self.df['x_max'] = self.df.apply(lambda row: 1 if row.class_id==14 else row.x_max, axis=1)
+        self.df['y_min'] = self.df.apply(lambda row: 0 if row.class_id==14 else row.y_min, axis=1)
+        self.df['y_max'] = self.df.apply(lambda row: 1 if row.class_id==14 else row.y_max, axis=1)
 
     def get_order_classes(self) -> list:
         df = self.df
@@ -134,7 +143,6 @@ class CocoDataset:
                 }
                 my_dict['categories'].append(class_dict)
 
-
         annotations = [
             row
             for row in zip(
@@ -164,7 +172,19 @@ class CocoDataset:
                     }
                     my_dict['images'].append(img_dict)
 
-                ann_w, ann_h = xmax - xmin, ymax- ymin
+                ratio = resized_ratio(
+                    raw_shape = (width, height),
+                    target_size = (self.img_size, self.img_size),
+                    keep_ratio=self.keep_ratio,
+                )
+
+                # Resize bbox
+                xmax = xmax*ratio[0]
+                xmin = xmin*ratio[0]
+                ymax = ymax*ratio[1]
+                ymin = ymin*ratio[1]
+                ann_w = xmax-xmin
+                ann_h = ymax-ymin
                 image_id = image_dict[image_name]
                 ann_dict = {
                     'id': item_count,
@@ -238,17 +258,25 @@ def get_bboxes_xyxy(
     annotations = [
         row
         for row in zip(
-            df["class_id"], df["x_min"], df["y_min"], df["x_max"], df["y_max"]
+            df["class_id"], df["x_min"], df["y_min"], df["x_max"], df["y_max"], df["rad_id"]
         )
     ]
     return annotations
 
+def get_image_info(
+    img_id: str,
+    database: pd.DataFrame,
+) -> list:
+    database = database.loc[database.image_id == img_id].head(1)
+    base_size = int(database.width), int(database.height)
+    return base_size
 
 def get_bboxes_xyxy_resized(
     size: int,
     img_id: str,
     database: pd.DataFrame,
     ignore_classes: list = IGNORE_CLASSES,
+    keep_ratio: bool = True
 ) -> list:
     bboxes = get_bboxes_xyxy(
         img_id=img_id, database=database, ignore_classes=ignore_classes
@@ -256,10 +284,10 @@ def get_bboxes_xyxy_resized(
     database = database.loc[database.image_id == img_id].head(1)
     base_size = int(database.width), int(database.height)
     target_size = size, size
-    ratio = resized_ratio(raw_shape=base_size, target_size=target_size, keep_ratio=True)
+    ratio = resized_ratio(raw_shape=base_size, target_size=target_size, keep_ratio=keep_ratio)
     annotations = [
-        (id, xmin * ratio[0], ymin * ratio[1], xmax * ratio[0], ymax * ratio[1])
-        for id, xmin, ymin, xmax, ymax in bboxes
+        (id, xmin * ratio[0], ymin * ratio[1], xmax * ratio[0], ymax * ratio[1], rad_id)
+        for id, xmin, ymin, xmax, ymax, rad_id in bboxes
     ]
 
     return annotations
