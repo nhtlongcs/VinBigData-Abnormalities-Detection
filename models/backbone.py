fix multi gpus, mixed precision

models/backbone.py
@@ -23,12 +23,11 @@ class EfficientDetBackbone(nn.Module):
 
         self.model = DetBenchTrain(net, config)
 
-
     def forward(self, inputs, targets):
         return self.model(inputs, targets)
 
     def detect(self, inputs, img_sizes, img_scales, conf_threshold=0.2):
-        outputs = self.model.detect(inputs, img_sizes, img_scales)
+        outputs = self.model(inputs, inference=True, img_sizes=img_sizes, img_scales=img_scales)
         outputs = outputs.cpu().numpy()
         out = []
         for i, output in enumerate(outputs):
@@ -54,3 +53,4 @@ class EfficientDetBackbone(nn.Module):
                 })
 
         return out
+    

models/detector.py
@@ -12,8 +12,7 @@ class Detector(BaseModel):
     def __init__(self, model, n_classes, **kwargs):
         super(Detector, self).__init__(**kwargs)
         self.model = model
-        self.set_dataparallel()
-        self.model_name = self.model.module.name
+        self.model_name = self.model.name
         if self.optimizer is not None:
             self.optimizer = self.optimizer(self.parameters(), lr= self.lr)
             self.set_optimizer_params()
@@ -88,12 +87,4 @@ class Detector(BaseModel):
             outputs = self(inputs)
         return outputs
 
-    def set_dataparallel(self):
-        self.model = CustomDataParallel(self.model)
     
-class CustomDataParallel(nn.DataParallel):
-    def __init__(self, model):
-        super(CustomDataParallel, self).__init__(module=model)
-    
-    def detect(self, inputs, img_sizes, img_scales, conf_threshold=0.2):
-        return self.module.detect(inputs, img_sizes, img_scales, conf_threshold)

models/effdet/bench.py
@@ -119,34 +119,41 @@ class DetBenchTrain(nn.Module):
             self.anchor_labeler = AnchorLabeler(self.anchors, self.num_classes, match_threshold=0.5)
         self.loss_fn = DetectionLoss(model.config)
 
-    def forward(self, x, target: Dict[str, torch.Tensor]):
+        # Distributed model on multi-gpus
+        self.model = nn.DataParallel(self.model).cuda()
+        self.loss_fn = nn.DataParallel(self.loss_fn).cuda()
+
+    def forward(self, x, target=None, inference=False, img_sizes=None, img_scales=None):
         class_out, box_out = self.model(x)
-        if self.anchor_labeler is None:
-            # target should contain pre-computed anchor labels if labeler not present in bench
-            assert 'label_num_positives' in target
-            cls_targets = [target[f'label_cls_{l}'] for l in range(self.num_levels)]
-            box_targets = [target[f'label_bbox_{l}'] for l in range(self.num_levels)]
-            num_positives = target['label_num_positives']
+        if inference == False:
+            if self.anchor_labeler is None:
+                # target should contain pre-computed anchor labels if labeler not present in bench
+                assert 'label_num_positives' in target
+                cls_targets = [target[f'label_cls_{l}'] for l in range(self.num_levels)]
+                box_targets = [target[f'label_bbox_{l}'] for l in range(self.num_levels)]
+                num_positives = target['label_num_positives']
+            else:
+                cls_targets, box_targets, num_positives = self.anchor_labeler.batch_label_anchors(
+                    target['boxes'], target['labels'])
+            
+            loss, class_loss, box_loss = self.loss_fn(class_out, box_out, cls_targets, box_targets, num_positives)
+
+            loss = loss.mean()
+            class_loss = class_loss.mean()
+            box_loss = box_loss.mean()
+            
+            output = {'T': loss, 'C': class_loss, 'B': box_loss}
+            return output
         else:
-            cls_targets, box_targets, num_positives = self.anchor_labeler.batch_label_anchors(
-                target['boxes'], target['labels'])
-
-        loss, class_loss, box_loss = self.loss_fn(class_out, box_out, cls_targets, box_targets, num_positives)
-        output = {'T': loss, 'C': class_loss, 'B': box_loss}
-
-        return output
-
-    def detect(self, x, img_sizes, img_scales):
-        class_out, box_out = self.model(x)
-        # if eval mode, output detections for evaluation
-        class_out_pp, box_out_pp, indices, classes = _post_process(
-            class_out, box_out, num_levels=self.num_levels, num_classes=self.num_classes,
-            max_detection_points=self.max_detection_points)
-        detection = _batch_detection(
-            x.shape[0], class_out_pp, box_out_pp, self.anchors.boxes, indices, classes,
-            img_scales, img_sizes,
-            max_det_per_image=self.max_det_per_image, soft_nms=self.soft_nms)
-        return detection
+            class_out_pp, box_out_pp, indices, classes = _post_process(
+                class_out, box_out, num_levels=self.num_levels, num_classes=self.num_classes,
+                max_detection_points=self.max_detection_points)
+            detection = _batch_detection(
+                x.shape[0], class_out_pp, box_out_pp, self.anchors.boxes, indices, classes,
+                img_scales, img_sizes,
+                max_det_per_image=self.max_det_per_image, soft_nms=self.soft_nms)
+            return detection
+        
 
 
 def unwrap_bench(model):

train.py
@@ -8,7 +8,10 @@ torch.backends.cudnn.fastest = True
 seed_everything()
 
 def train(args, config):
-    device = torch.device(config.devices if torch.cuda.is_available() else 'cpu')
+    os.environ['CUDA_VISIBLE_DEVICES'] = config.gpu_devices
+    num_gpus = len(config.gpu_devices.split(','))
+
+    device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
     train_transforms = get_augmentation(config, _type = 'train')
     val_transforms = get_augmentation(config, _type = 'val')
@@ -113,6 +116,7 @@ def train(args, config):
     print(trainer)
     print()
     print(config)
+    print(f'Training with {num_gpus} gpu(s)')
     print(f"Start training at [{start_epoch}|{start_iter}]")
     print(f"Current best MAP: {best_value}")
     

trainer/trainer.py
@@ -8,11 +8,7 @@ from loggers.loggers import Logger
 from utils.utils import clip_gradient
 import time
 from utils.utils import box_nms_numpy, change_box_order, draw_pred_gt_boxes
-try:
-    from apex import amp
-    apex_is_imported = True
-except ImportError:
-    apex_is_imported = False
+from torch.cuda import amp
 
 class Trainer():
     def __init__(self,
@@ -86,15 +82,14 @@ class Trainer():
         for i, batch in enumerate(self.trainloader):
             
             start_time = time.time()
-            loss, loss_dict = self.model.training_step(batch)
-
-            if loss.item() == 0 or not torch.isfinite(loss):
-                continue
+            
             
             if self.use_amp:
-                with amp.scale_loss(loss, self.optimizer) as scaled_loss:
-                    scaled_loss.backward()
+                with amp.autocast():
+                    loss, loss_dict = self.model.training_step(batch)
+                    self.scaler.scale(loss).backward()
             else:
+                loss, loss_dict = self.model.training_step(batch)
                 loss.backward()
             
             if self.clip_grad is not None:
@@ -102,10 +97,18 @@ class Trainer():
 
             if self.use_accumulate:
                 if (i+1) % self.accumulate_steps == 0 or i == len(self.trainloader)-1:
-                    self.optimizer.step()
+                    if self.use_amp:
+                        self.scaler.step(self.optimizer)
+                        self.scaler.update()
+                    else:
+                        self.optimizer.step()
                     self.optimizer.zero_grad()
             else:
-                self.optimizer.step()
+                if self.use_amp:
+                    self.scaler.step(self.optimizer)
+                    self.scaler.update()
+                else:
+                    self.optimizer.step()
                 self.optimizer.zero_grad()
 
             end_time = time.time()
@@ -264,9 +267,9 @@ class Trainer():
 
     def set_amp(self):
         self.use_amp = False
-        if self.cfg.mixed_precision and apex_is_imported:
+        if self.cfg.mixed_precision:
             self.use_amp = True
-            self.model, self.optimizer = amp.initialize(self.model, self.optimizer, opt_level="O1", verbosity=0)
+            self.scaler = amp.GradScaler()
 
     def forward_test(self):
         self.model.eval()