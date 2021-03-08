d48b895aca713b4e90c5664dfa6daeb5c1e34fb5
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2021-03-08 22:13:54 +0700

    add img size arg

--- scripts/prepare-data/2-convert-folds.sh
+++ scripts/prepare-data/2-convert-folds.sh
@@ -1,7 +1,8 @@
+img_size=1024
 for x in 0 1 2 3 4
 do 
-    python scripts/convert-format/convert.py --resize 1024 --ignored [14] --csv data/raw/folds/${x}/${x}_train.csv --format coco --output_path data/detection/coco/${x}/
-    python scripts/convert-format/convert.py --resize 1024 --ignored [] --csv data/raw/folds/${x}/${x}_val.csv --format coco --output_path data/detection/coco/${x}/
-    # python scripts/convert-format/convert.py --resize 1024 --ignored [14] --csv data/raw/folds/${x}/${x}_train.csv --format yolo --output_path data/detection/yolo/${x}/train/
-    # python scripts/convert-format/convert.py --resize 1024 --ignored [] --csv data/raw/folds/${x}/${x}_val.csv --format yolo --output_path data/detection/yolo/${x}/val/
+    python scripts/convert-format/convert.py --resize ${img_size} --ignored [14] --csv data/raw/folds/${x}/${x}_train.csv --format coco --output_path data/detection/${img_size}/coco/${x}/
+    python scripts/convert-format/convert.py --resize ${img_size} --ignored [] --csv data/raw/folds/${x}/${x}_val.csv --format coco --output_path data/detection/${img_size}/coco/${x}/
+    python scripts/convert-format/convert.py --resize ${img_size} --ignored [14] --csv data/raw/folds/${x}/${x}_train.csv --format yolo --output_path data/detection/${img_size}/yolo/${x}/train/
+    python scripts/convert-format/convert.py --resize ${img_size} --ignored [] --csv data/raw/folds/${x}/${x}_val.csv --format yolo --output_path data/detection/${img_size}/yolo/${x}/val/
 done
