b8afcf429a8855821005a7b7ce79f6168bb1fc56
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2021-03-09 00:47:07 +0700

    revert meta data and finalize scripts

--- scripts/merge-bboxes/README.md
+++ scripts/merge-bboxes/README.md
@@ -9,8 +9,8 @@ python merge.py --csv_in=train.csv --type=wbf
 - ***Parameters***:
     - ***--csv_in***:          path to csv file
     - ***--csv_out***:         path to ouptut csv file
-    - ***--skip_threshold***:  iou threshold to remove overlappings
-    - ***--iou_threshold***:   skip box whose confidence lower than threshold (for WBF only)
+    - ***--skip_threshold***:   skip box whose confidence lower than threshold (for WBF only)
+    - ***--iou_threshold***:   iou threshold to remove overlappings
     - ***--type***:            box fusion method, support ['nms', 'wbf']
     - ***--class_mapping***:   class name csv file
     - ***--ignored_classes***: string of list of ignored classes. Example: '[14, 15]'
--- scripts/prepare-data/2-convert-folds.sh
+++
@@ -1,8 +0,0 @@
-img_size=1024
-for x in 0 1 2 3 4
-do 
-    python scripts/convert-format/convert.py --resize ${img_size} --ignored [14] --csv data/raw/folds/${x}/${x}_train.csv --format coco --output_path data/detection/${img_size}/coco/${x}/
-    python scripts/convert-format/convert.py --resize ${img_size} --ignored [] --csv data/raw/folds/${x}/${x}_val.csv --format coco --output_path data/detection/${img_size}/coco/${x}/
-    python scripts/convert-format/convert.py --resize ${img_size} --ignored [14] --csv data/raw/folds/${x}/${x}_train.csv --format yolo --output_path data/detection/${img_size}/yolo/${x}/train/
-    python scripts/convert-format/convert.py --resize ${img_size} --ignored [] --csv data/raw/folds/${x}/${x}_val.csv --format yolo --output_path data/detection/${img_size}/yolo/${x}/val/
-done
