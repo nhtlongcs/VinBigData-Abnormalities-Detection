2181c749553509774757bcedde2ac2cdb3cecdc3
nhtlongcs <nhtlong@selab.hcmus.edu.vn>
2021-03-10 16:31:55 +0700

    refactor something

--- scripts/merge-bboxes/merge-special-cls.py
+++ scripts/merge-bboxes/merge-special-cls.py
@@ -42,6 +42,32 @@ def calc_conf(row, count_df, data_df, classes=[0, 3, 14], nor=3):
     return row
 
 
+def clean(csv: pd.DataFrame) -> tuple:
+
+    process_csv = csv[(csv.class_id == 0) | (csv.class_id == 3) | (csv.class_id == 14)]
+    ignore_csv = csv[(csv.class_id != 0) & (csv.class_id != 3) & (csv.class_id != 14)]
+    process_csv = process_csv.drop_duplicates(
+        subset=["image_id", "class_id"], keep="last"
+    )
+
+    rad_count_df = csv.groupby(["image_id", "class_id"]).rad_id.count()
+    data_df = csv.groupby(["image_id", "class_id"])
+
+    process_csv = process_csv.progress_apply(
+        lambda row: calc_conf(row, rad_count_df, data_df), axis=1
+    )
+    for df in [process_csv, ignore_csv]:
+        df[["x_min", "y_min", "x_max", "y_max"]] = csv[
+            ["x_min", "y_min", "x_max", "y_max"]
+        ].astype(int)
+
+    import pdb
+
+    pdb.set_trace()
+
+    return process_csv, ignore_csv
+
+
 if __name__ == "__main__":
     tqdm.pandas()
 
@@ -52,24 +78,7 @@ if __name__ == "__main__":
 
     csv["score"] = 1
 
-    process_csv = csv[(csv.class_id == 0) | (csv.class_id == 3) | (csv.class_id == 14)]
-    process_csv = process_csv.drop_duplicates(
-        subset=["image_id", "class_id"], keep="last"
-    )
-
-    ignore_csv = csv[(csv.class_id != 0) & (csv.class_id != 3) & (csv.class_id != 14)]
-
-    rad_count_df = csv.groupby(["image_id", "class_id"]).rad_id.count()
-    data_df = csv.groupby(["image_id", "class_id"])
-
-    process_csv = process_csv.progress_apply(
-        lambda row: calc_conf(row, rad_count_df, data_df), axis=1
-    )
-    final_csv = pd.concat([process_csv, ignore_csv])
-
-    final_csv[["x_min", "y_min", "x_max", "y_max"]] = final_csv[
-        ["x_min", "y_min", "x_max", "y_max"]
-    ].astype(int)
+    final_csv = pd.concat([*clean(csv)])
 
     print(f"droped {len(csv) - len(final_csv)} rows")
 
